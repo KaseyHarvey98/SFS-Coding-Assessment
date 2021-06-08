e86aeff487d1cdaf70e99b69a2a700e9e53d14a7
KaseyHarvey98 <42356469+KaseyHarvey98@users.noreply.github.com>
2021-06-08 18:43:16 -0500

    re-did lost changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState} from "react";
+import Table from "./components/Table";
+import Total from "./components/Total";
+import DebtButtons from "./components/DebtButtons";
+import AddDebt from "./components/AddDebt";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [popUp, setPopUp] = useState(false);
+	// made functions b/c would loop infinitely otherwise
+	const open = () => {
+		setPopUp(true);
+	};
+	const close = () => {
+		setPopUp(false);
+	};
+	return (
+		<div className="App">
+			<Table />
+			{popUp ? <AddDebt close={close} /> : null}
+			<DebtButtons open={open} />
+			<Total />
+		</div>
+	);
 }
 
 export default App;
